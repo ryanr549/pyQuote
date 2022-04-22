0550cf2b76fa6b24f05342508293f97b0b45b814
ryanr549 <ryanr@mail.ustc.edu.cn>
2022-04-23 00:44:53 +0800

    bug fix

--- pyquote/scrap.py
+++ pyquote/scrap.py
@@ -7,8 +7,8 @@ from bs4 import BeautifulSoup
 
 def scrap_quotes(subject):
     """subject: topic you'd like to search in wikiquote site"""
-    headers = {'user-agent':
-               'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'}
+    # headers = {'user-agent':
+    #           'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'}
     # specify header as linux mozilla firefox browser
     url = 'https://en.wikiquote.org/wiki/' + subject
     print(url)
@@ -25,26 +25,24 @@ def scrap_quotes(subject):
 
 def scrap_unsplash(keyword):
     """keyword: used to find pics on unsplash.com"""
-    headers = {'user-agent':
-               'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'}
+    # headers = {'user-agent':
+    #            'Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0'}
     # specify header as linux mozilla firefox browser
-    url = 'https://unsplash.com/s/photos/' + keyword +'?orientation=landscape&color=black'
+    url = 'https://unsplash.com/s/photos/' + keyword + '?orientation=landscape&color=black'
     bs = BeautifulSoup(requests.get(url).text)
     search = bs.find("div", {"data-test": "search-photos-route"})
     images = search.findAll("figure", {"itemprop": "image"})[0:16]
+    if not len(images):
+        url = 'https://unsplash.com/s/photos/' + keyword + '?orientation=landscape&color=white'
+        bs = BeautifulSoup(requests.get(url).text)
+        search = bs.find("div", {"data-test": "search-photos-route"})
+        images = search.findAll("figure", {"itemprop": "image"})[0:16]
     image_list = []
     for image in images:
         # find images source url in the search result page
-        href = image.find("a", {"itemprop": "contentUrl"})['href']
-        src_page_url = 'https://unsplash.com' + href
-        src_bs = BeautifulSoup(requests.get(src_page_url).text)
-        photo_panel = src_bs.find("div", {"data-test": "photos-route"})
-        # since the image link is next to the only jscript in the page
-        # first find the script then use parent attribute to locate
-        # the high-resolution image url
-        srcset = photo_panel.find("script").parent.find("img")['srcset']
+        srcset = image.find("img")['srcset']
         # use regular expression to extract a 2k image source url
-        pattern = re.compile(r'https?:\/\/[\w.\/\-?=&]+\s2[0-9]+w')
+        pattern = re.compile(r'https?:\/\/[\w.\/\-?=&%]+\s2[0-9][0-9][0-9]w')
         src, _, _ = pattern.search(srcset).group().partition(' ')
         # extract image_url from the string using partition() method
         image_list.append(src)
